3623a331aa2753be01bcb13cd5b291974d206569
edde746 <g@g.g>
2022-01-13 14:58:04 +0100

    Adjust Parrot Explosion

--- Assets/Scripts/ParrotScript.cs
+++ Assets/Scripts/ParrotScript.cs
@@ -6,7 +6,7 @@ public class ParrotScript : ShotBehaviour
 {
     public float lifetime = 2.0f;
     public float explosionRadius = 5.0f;
-    public float explosionStrength = 14.0f;
+    public float explosionStrength = 20.0f;
     public GameObject explosionParticle;
 
     void Update()
